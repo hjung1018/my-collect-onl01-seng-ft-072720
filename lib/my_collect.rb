20bc5d3d9ff26c3085cd3fa0c115d496ba104965
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-04 00:29:28 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array) 
+    counter = 0
+    new_array = []
+      while counter < array.length
+      
+       new_array <<  yield(array[counter])
+       counter += 1
+    
+   end
+  return new_array
+end
